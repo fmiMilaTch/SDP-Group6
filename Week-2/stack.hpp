b59f7e52fb72cd837f62b6bfa831a71c112298ee
fmiMilaTch <104836126+fmiMilaTch@users.noreply.github.com>
2023-10-10 10:59:08 +0300

    Add files via upload

---
+++ Week-2/stack.hpp
@@ -0,0 +1,134 @@
+#ifndef _STACK_HPP
+#define _STACK_HPP
+
+#include <iostream>
+#include <cstring>
+
+template<typename T>
+class Stack {
+private:
+    T* elements;
+    size_t size;
+    size_t capacity;
+
+    void clear();
+    void scale(size_t capacity);
+    void copy(const Stack<T>& other);
+
+public:
+
+    Stack(size_t capacity = 64);
+    Stack(const Stack<T>& other);
+    Stack& operator=(const Stack<T>& other);
+    ~Stack();
+
+    T Pop();
+    T Peek() const;
+    void Push(const T& element);
+
+    bool Empty() const;
+    size_t Size() const;
+
+    template<typename E>
+    friend std::ostream& operator<<(std::ostream& out, const Stack<E>& stack);
+};
+
+template<typename T>
+void Stack<T>::clear() {
+    this->size = 0;
+    this->capacity = 0;
+    delete this->elements;
+}
+
+
+template<typename T>
+void Stack<T>::scale(size_t capacity) {
+    T* elements = new T[capacity];
+
+    memcpy(elements, this->elements, this->size * sizeof(T));
+
+    delete this->elements;
+    this->capacity = capacity;
+    this->elements = elements;
+}
+
+template<typename T>
+void Stack<T>::copy(const Stack<T>& other) {
+    this->size = other.size;
+    this->capacity = other.capacity;
+    this->elements = new T[this->capacity];
+
+    memcpy(this->elements, other.elements, this->size * sizeof(T));
+}
+
+template<typename T>
+Stack<T>::Stack(size_t capacity) {
+    this->size = 0;
+    this->capacity = capacity;
+    this->elements = new T[capacity];
+}
+
+template<typename T>
+Stack<T>::Stack(const Stack<T>& other) {
+    this->copy(other);
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack<T>& other) {
+    if(this != &other) {
+        this->clear();
+        this->copy(other);
+    }
+
+    return *this;
+
+}
+
+template<typename T>
+Stack<T>::~Stack() {
+    this->clear();
+}
+
+template<typename T>
+void Stack<T>::Push(const T& element) {
+    if (this->size == this->capacity) {
+        this->scale(this->capacity * 2);
+    }
+
+    this->elements[this->size++] = element;
+}
+
+template<typename T>
+T Stack<T>::Pop() {
+    if ((this->size-1) == this->capacity / 2) {
+        this->scale(this->capacity / 2);
+    }
+
+    return this->elements[this->size--];
+}
+
+template<typename T>
+T Stack<T>::Peek() const {
+    return this->elements[this->size];
+}
+
+template<typename T>
+size_t Stack<T>::Size() const {
+    return this->size;
+}
+
+template<typename T>
+bool Stack<T>::Empty() const {
+    return this->size == 0;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const Stack<T>& stack) {
+    for(size_t idx = 0; idx < stack.size; idx++) {
+        out << stack.elements[idx] << " ";
+    }
+
+    return out;
+}
+
+#endif
